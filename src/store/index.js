ec1b0edf4540c99da33f082276ca181c4c50a882
Santiago Cabrera <45210638+stiagocab@users.noreply.github.com>
2021-12-07 19:20:40 -0600

    ADD: store persitente para favoritos

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,37 @@
 import { configureStore } from "@reduxjs/toolkit";
+import throttle from "lodash/throttle";
+
 import rootReducers from "./reducers";
+
+
+// PERSIST STORE
+const saveState = (state) => {
+  // PREVENT SAVE
+
+  try {
+    const serializedState = JSON.stringify(state);
+
+    localStorage.setItem("state", serializedState);
+  } catch (err) {
+    // Ignorar
+  }
+};
+
+const loadState = () => {
+  try {
+    const serializeState = localStorage.getItem("state");
+    if (serializeState === null) {
+      return {};
+    }
+    return JSON.parse(serializeState);
+  } catch (err) {
+    return {};
+  }
+};
+
+
+const preloadedState = loadState();
+
 
 const store = configureStore({
   reducer: rootReducers,
@@ -7,6 +39,14 @@ const store = configureStore({
   devTools:
     process.env.NODE_ENV !== "production" &&
     window.__REDUX_DEVTOOLS_EXTENSION__,
+  preloadedState,
 });
 
 export default store;
+
+
+store.subscribe(
+  throttle(() => {
+    saveState(store.getState());
+  }, 1000)
+);
